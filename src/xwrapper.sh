8e7ee5ba9c9b572a823aa8600ddd7320875377ce
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2023-05-05 13:34:27 -0500

    wrap

--- src/xwrapper.sh
+++ src/xwrapper.sh
@@ -3,7 +3,5 @@
 # Run compiled SPM in virtual X.
 
 xvfb-run --server-num=$(($$ + 99)) \
---server-args='-screen 0 1600x1200x24 -ac +extension GLX' \
-/opt/makerois/bin/run_spm12.sh \
-/usr/local/MATLAB/MATLAB_Runtime/v97 \
-"$@"
+    --server-args='-screen 0 1600x1200x24 -ac +extension GLX' \
+    makerois.sh "$@"
